169d8bf5541a4a9774127c688f05666443db7a28
Doug Leung <dleung46@mylangara.ca>
2021-06-24 22:17:56 -0700

    To code firebase register, login and logout

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -1,3 +1,4 @@
+//**************************************************************/
 // Your web app's Firebase configuration
     // For Firebase JS SDK v7.20.0 and later, measurementId is optional
     var firebaseConfig = {
@@ -13,20 +14,95 @@
     firebase.initializeApp(firebaseConfig);
     firebase.analytics();
 
+//********** Register page**************************************/
+// Register / Sign up new user //
+
+//********** Register page**************************************/
+// declaring variables from HTML elements.
+
+const usernameInput = document.querySelector('#username');
+const emailInput = document.querySelector('#email');
+const passwordInput = document.querySelector('#password');
+const confirmPassword = document.querySelector('#confirmPassword');
+const registerBtn = document.querySelector('#registerButton');
+const registerForm = document.querySelector('#registerForm');
+
+// collecting data into Object
+
+class userProfile {
+    constructor(name, email, pw, pw_2) {
+        this.userName = name;
+        this.email = email;
+        this.password = pw;
+        this.confirmPassword = pw_2;
+        this.passwordMatch = function () {
+            if (this.password !== this.confirmPassword) {
+                document.getElementById('outputMessage').innerHTML = "Your password does not match.  Please try again.";
+            } else {
+                console.log('Password matched.');
+            }
+        }
+        this.passwordMatch();
+    }
+
+    get userName() {
+        return this._userName.charAt(0).toUpperCase() + this._userName.slice(1);
+    }
+    set userName(userInput) {
+        if (userInput.length < 2 || typeof userInput !== "string") {
+            document.getElementById('outputMessage').innerHTML = "USERNAME SHOULD BE AT LEAST 2 CHARACTERS LONG and SHOULD CONTAIN LETTERS ONLY";
+            this._userName = userInput;
+        }
+    }
+
+    get password() {
+        return this._password;
+    }
+    set password(pw) {
+        // Code for password validation taken from  https://www.geeksforgeeks.org/validate-a-password-using-html-and-javascript/
+        if (pw.match(/[a-z]/g) && pw.match(
+            /[0-9]/g) && pw.match(
+                /[^a-zA-Z\d]/g) && pw.length >= 8) {
+            this._password = pw;
+        } else { document.getElementById('outputMessage').innerHTML = "Password should have at least 1 lowercase letter; 1 digit, 1 special character and be minium 8 characters"; 
+        }
+    }
+}
+
+registerForm.addEventListener('submit', (event) => {
+    event.preventDefault(); //MUST REMOVE THIS FOR DEPLOYMENT!!!!!!!!
+    console.log('Form submitted')
+    let newUser = new userProfile(usernameInput.value, emailInput.value, passwordInput.value, confirmPassword.value);
+
+    firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
+    .then((user) => {
+        if(user) {
+            alert('Welcome to 8Wishes! You are now a registered user!');
+        }
+    })
+    .catch((error) => {
+      let errorMessage = error.message;
+
+          console.log('Error registering, ', errorMessage);
+          alert(errorMessage);
+    
+    });
+})
+
+//**************************************************************/
+// Log/Sign In and Sign Out
+
     //Get the currently signed in user (whether logged in or not)
     firebase.auth().onAuthStateChanged((user) => {
         let notLoggedIn = document.getElementById('not-logged-in');
         let loggedIn = document.getElementById('logged-in');
         if (user) {
-          // User is signed in, see docs for a list of available properties
-          // https://firebase.google.com/docs/reference/js/firebase.User
             loggedIn.style.display = 'block';
             notLoggedIn.style.display = 'none';
 
         // Get User Profile
         const user = firebase.auth().currentUser;
             if (user !== null) {
-              // The user object has basic properties such as display name, email, etc.
             // Part of registration form 
             // const displayName = user.displayName;
               const email = user.email;
@@ -37,7 +113,9 @@
                 loggedIn.style.display = 'none';
                 notLoggedIn.style.display = 'block';
             }
-      });
+    });
+
+    // Logging in
 
     document.getElementById("btn_login").addEventListener("click", () => {
         let email = document.getElementById('email1').value;
@@ -51,12 +129,13 @@
         })
         .then((user) => {
             if(user) {
-
                 // We can have a message on the DOM, rather than alert
                 alert('Welcome back.  You are now logged in!');
             }
         })
     })
+
+    // Logging out
 
     document.getElementById("btn_logout").addEventListener("click", () => {
         firebase.auth().signOut().then(() => {
@@ -64,6 +143,8 @@
           }).catch((error) => {
             // An error happened.
           });
+
+            // Housekeeping.
           document.getElementById('email1').value = "";
           document.getElementById('password1').value = "";
           document.getElementById('email1').focus();
