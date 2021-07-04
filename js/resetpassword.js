d1d7db521a9ba5b9bae909e76f5e2119485e7c21
Doug Leung <dleung46@mylangara.ca>
2021-07-03 22:37:53 -0700

    add button to take back to login page

--- js/resetpassword.js
+++ js/resetpassword.js
@@ -4,10 +4,16 @@ resetPassword.addEventListener('click', () => {
 
     firebase.auth().sendPasswordResetEmail(emailtoreset.value)
     .then(() => {
-    alert('Password is reset. Check your e-mail!');
+    console.log('Password is reset. Check your e-mail!');
     })
     .catch((error) => {
             console.error('Error sending reset password e-mail, ', error.message);
             alert(error.message);
     })
+
+    document.getElementById('resetfeedback').innerHTML = "Your password is reset. Please check your e-mail."
+    emailtoreset.value = "";
+    emailtoreset.focus();
+    noreset.style.display = "none";
 });
+
