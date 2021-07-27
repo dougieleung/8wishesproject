8383c93dab485c270f17699c05f21d592854b91e
Doug Leung <dleung46@mylangara.ca>
2021-07-26 17:39:36 -0700

    199: onboarding demo; video & welcome msg

--- js/pageDisplayIntro.js
+++ js/pageDisplayIntro.js
@@ -11,35 +11,56 @@ const welcomePage = document.querySelector("#welcomePage");
 const exploreLink = document.querySelector("#exploreLink");
 const seeDemo = document.querySelector("#seeDemo");
 const demoPage = document.querySelector("#demo-page");
-const signUpBtn = document.querySelector("#sign-up");
-const loginBtn = document.querySelector("#log-in");
+const demopage1 = document.querySelector("#demopage1");
+const demopage2 = document.querySelector("#demopage2");
+const demopage3 = document.querySelector("#demopage3");
+const ellipsis1 = document.querySelector("#ellipsis1");
+const ellipsis2 = document.querySelector("#ellipsis2");
+const ellipsis3 = document.querySelector("#ellipsis3");
+// const signUpBtn = document.querySelector("#sign-up");
+// const loginBtn = document.querySelector("#log-in");
 const loginPage = document.querySelector("#loginPage");
 const loginLink = document.querySelector("#loginLink");
 const registerLink = document.querySelector("#registerLink");
 const skiptologin = document.querySelector("#skiptologin");
 
-
 seeDemo.addEventListener("click", () => {
-
     welcomePage.classList.remove('show');
     welcomePage.classList.add('hide');
     demoPage.classList.toggle('hide');
-
 })
 
-signUpBtn.addEventListener("click", () => {
-
-    demoPage.classList.remove('show');
-    demoPage.classList.add('hide');
-    registerPage.classList.toggle('hide');
-
+ellipsis1.addEventListener("click", () => {
+    demopage1.classList.remove('show');
+    demopage1.classList.add('hide');
+    demopage2.classList.toggle('hide');
 })
 
-loginBtn.addEventListener("click", () => {
-    demoPage.classList.remove('show');
-    demoPage.classList.add('hide');
-    loginPage.classList.toggle('hide');
+ellipsis2.addEventListener("click", () => {
+    demopage2.classList.remove('show');
+    demopage2.classList.add('hide');
+    demopage3.classList.toggle('hide');
 })
+
+ellipsis3.addEventListener("click", () => {
+    demopage3.classList.remove('show');
+    demopage3.classList.add('hide');
+    demopage1.classList.toggle('hide');
+})
+
+// signUpBtn.addEventListener("click", () => {
+
+//     demoPage.classList.remove('show');
+//     demoPage.classList.add('hide');
+//     registerPage.classList.toggle('hide');
+
+// })
+
+// loginBtn.addEventListener("click", () => {
+//     demoPage.classList.remove('show');
+//     demoPage.classList.add('hide');
+//     loginPage.classList.toggle('hide');
+// })
 
 skiptologin.addEventListener("click", () => {
     welcomePage.classList.remove('show');
